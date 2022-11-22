fc43533cb0c05c22641362b828c3155cf223a756
Lucas Maia <Lucasdiomaia@gmail.com>
2022-11-22 00:18:43 -0300

    Versionamento e criação do menu

---
+++ CursoPoo/Menu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+
+namespace CursoPoo
+{
+    public class Menu
+    { 
+        public void MenuPrincipal()
+        {
+            
+            var loop = true;
+            while (loop)
+            {
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("Escolha uma das opções abaixo");
+                Console.WriteLine("1 - Gerenciar Alunos");
+                Console.WriteLine("2 - Gerenciar Disciplinas");
+                Console.WriteLine("3 - Gerenciar Cursos");
+                Console.WriteLine("4 - Sair");
+                Console.WriteLine("Digite a opção desejada: ");
+                Console.WriteLine("-----------------------------------");
+                
+                try
+                {
+                    var opcao = int.Parse(Console.ReadLine());
+                    switch (opcao)
+                    {
+                        case 1:
+                            Console.WriteLine("Gerenciar Alunos");
+                            //GerenciarAluno();
+                            break;
+                        case 2:
+                            Console.WriteLine("Gerenciar Disciplinas");
+                            //MenuDiciplinas();
+                            break;
+                        case 3:
+                            Console.WriteLine("Gerenciar Cursos");
+                            // Cursos();
+                            break;
+                        case 4:
+                            Console.WriteLine("Saindo...");
+                            loop = false;
+                            break;
+                        default:
+                            Console.WriteLine("Opção inválida, coloque um número de 1 a 4");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Opção inválida, coloque um número de 1 a 4");
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ CursoPoo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CursoPoo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        
+        {
+            Menu menu = new Menu();
+            menu.MenuPrincipal();
+        }
+            
+    }
+}
